cdd6555af62a28b61da792bafbac1d0a001492f4
鲁文静 <2290806321@qq.com>
2017-12-17 21:10:35 +0800

    Update indexRedux.js

--- src/component/SubmitDialog/indexRedux.js
+++ src/component/SubmitDialog/indexRedux.js
@@ -11,30 +11,3 @@ export function addTask (task) {
 	}
 }
 
-//处理添加action的reducer;
-function addTaskReducer (state = { taskList: []}, action) {
-
-	let len = state.taskListData.taskList.length;
-	let index = len > 0 ? len - 1 : 0;
-	let lastTaskId = index !== 0 ? state.taskListData.taskList[index].id : 0; 
-
-	switch (action.type) {
-		case ADD_TASK: {
-			state.taskListData.taskList.push({
-				id: lastTaskId + 1,
-				name: action.payload.name,
-				status: false,
-			});
-			return {
-				...state,
-				taskList: state.taskListData.taskList,
-			}
-		} 
-
-		default: {
-			return state;
-		}
-	}
-}
-
-export default addTaskReducer;
